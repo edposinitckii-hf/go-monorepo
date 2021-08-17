378bc4b1a086caef39bb50058ff39d1601b7ebb7
edposinitckii <eduard.posinitckii@hellofresh.com>
2021-08-17 20:04:50 +0200

    makefile updated

--- Makefile.go.mk
+++ Makefile.go.mk
@@ -1,5 +1,8 @@
-test-unit:
+vendor:
+	go mod vendor
+
+test-unit: vendor
 	go test -v -tags unit -short -race ./...
 
-test-integration:
+test-integration: vendor
 	go test -v -run Integration -tags integration -race ./...
